fb37d452e91500d5fe1599fb701bdda98c31b37b
Harry Smith <smithhj1@aston.ac.uk>
2018-02-04 17:10:44 +0000

    Second

---
+++ Lab2/Assets/_Scripts/LaptopController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class LaptopController : MonoBehaviour {
+
+    public Text timeText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeText.text = string.Format("{0}:{1:00}", (int)Time.timeSinceLevelLoad / 60, (int)Time.timeSinceLevelLoad % 60);
+
+
+    }
+}
